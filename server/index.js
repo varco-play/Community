5a92c5fcd6cd96f57611962d32a35277218dee12
varco-play <privetchel2007@gmail.com>
2024-05-15 01:43:25 +0500

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: "http://localhost:5173"}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/server/auth", auth)

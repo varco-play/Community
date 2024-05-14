998df5cdb9846daa58bb54a96459184241e64b5d
varco-play <privetchel2007@gmail.com>
2024-05-15 01:57:14 +0500

    t

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
-import auth from "./routes/auth.js"
+import auth from "./routes/auth.js";
 import connectDB from "./db/connect.js";
 import "dotenv/config";
 
-
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({ credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/server/auth", auth)
+app.use("/api/server/auth", auth);
 
 const PORT = process.env.PORT || 8888;
 
